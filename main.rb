e30a3912462dbec08b8231753a8944b6868c7c88
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-26 10:47:05 -0700

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,26 @@
+
+# Marvish Chandra
+
+# based on yearly averages
+
+class disneylandAdventure
+  def averageJanuary:
+    monday = [45,27,33,24,24]
+    avgMonday = monday.sum
+    tuesday = [43,40,23,31]
+    avgTuesday = tuesday.sum
+    wednesday = [41,31,20,26]
+    avgWednesday = wednesday.sum
+    thursday = [40,29,21,27]
+    avgThursday = thursday.sum
+    friday = [29,25,25,40]
+    avgFriday = friday.sum
+    saturday = [37,31,34,30,37]
+    avgSaturday = saturday.sum
+    sunday = [42,27,37,29,30]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+end
+  def averageFebruary:
+    
