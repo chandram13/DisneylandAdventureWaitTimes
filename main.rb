5384f2cac20a065a7784fb3d36102f45a39c6d61
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-26 11:52:37 -0700

    Add files via upload

--- main.rb
+++ main.rb
@@ -21,6 +21,203 @@ class disneylandAdventure
     avgSunday = sunday.sum
     monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
     print(monthlyAvg)
-end
+  end
   def averageFebruary:
-    
+    monday = [41,28,50,34]
+    avgMonday = monday.sum
+    tuesday = [27,34,29,47]
+    avgTuesday = tuesday.sum
+    wednesday = [28,30,36,59]
+    avgWednesday = wednesday.sum
+    thursday = [32,39,45,49]
+    avgThursday = thursday.sum
+    friday = [36,28,44,32]
+    avgFriday = friday.sum
+    saturday = [39,31,45,37]
+    avgSaturday = saturday.sum
+    sunday = [36,33,48,34]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end 
+  def averageMarch:
+    monday = [40,42,44,34]
+    avgMonday = monday.sum
+    tuesday = [39,36,42,43,39]
+    avgTuesday = tuesday.sum
+    wednesday = [40,43,40,27,36]
+    avgWednesday = wednesday.sum
+    thursday = [43,30,38,27,30]
+    avgThursday = thursday.sum
+    friday = [33,29,32,30]
+    avgFriday = friday.sum
+    saturday = [35,33,48,38]
+    avgSaturday = saturday.sum
+    sunday = [35,30,34,32]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end 
+  def averageApril:
+    monday = [34,59,34,27]
+    avgMonday = monday.sum
+    tuesday = [39,56,39,35]
+    avgTuesday = tuesday.sum
+    wednesday = [33,59,37,36]
+    avgWednesday = wednesday.sum
+    thursday = [30,54,39,35]
+    avgThursday = thursday.sum
+    friday = [29,29,48,29,31]
+    avgFriday = friday.sum
+    saturday = [31,41,47,34,33]
+    avgSaturday = saturday.sum
+    sunday = [29,41,47,28]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
+  def averageMay:
+    monday = [32,34,38,38,40]
+    avgMonday = monday.sum
+    tuesday = [28,38,38,36,27]
+    avgTuesday = tuesday.sum
+    wednesday = [28,40,36,38]
+    avgWednesday = wednesday.sum
+    thursday = [31,32,37,30]
+    avgThursday = thursday.sum
+    friday = [30,31,34,37]
+    avgFriday = friday.sum
+    saturday = [37,43,45,39]
+    avgSaturday = saturday.sum
+    sunday = [31,31,31,35,40]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
+  def averageJune:
+    monday = [39,44,45,43]
+    avgMonday = monday.sum
+    tuesday = [37,50,44,36]
+    avgTuesday = tuesday.sum
+    wednesday = [28,39,50,46,43]
+    avgWednesday = wednesday.sum
+    thursday = [28,34,49,35,28]
+    avgThursday = thursday.sum
+    friday = [38,40,36,35]
+    avgFriday = friday.sum
+    saturday = [29,37,40,35]
+    avgSaturday = saturday.sum
+    sunday = [41,36,30,30]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
+  def averageJuly:
+    monday = [32,34,39,40]
+    avgMonday = monday.sum
+    tuesday = [34,47,47,48]
+    avgTuesday = tuesday.sum
+    wednesday = [38,42,47,45]
+    avgWednesday = wednesday.sum
+    thursday = [37,36,42,48]
+    avgThursday = thursday.sum
+    friday = [29,26,28,33,34]
+    avgFriday = friday.sum
+    saturday = [36,26,29,43,35]
+    avgSaturday = saturday.sum
+    sunday = [39,28,32,32,31]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
+  def averageAugust:
+    monday = [33,29,31,26,12]
+    avgMonday = monday.sum
+    tuesday = [46,41,32,32,10]
+    avgTuesday = tuesday.sum
+    wednesday = [45,29,31,35]
+    avgWednesday = wednesday.sum
+    thursday = [35,38,31,24]
+    avgThursday = thursday.sum
+    friday = [28,25,23,20]
+    avgFriday = friday.sum
+    saturday = [31,33,31,19]
+    avgSaturday = saturday.sum
+    sunday = [25,26,35,17]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
+  # remaining months used 2021 info, since program was written in Aug 2022
+  def averageSeptember:
+    monday = [22,17,21,27]
+    avgMonday = monday.sum
+    tuesday = [16,16,17,21]
+    avgTuesday = tuesday.sum
+    wednesday = [8,14,20,21,27]
+    avgWednesday = wednesday.sum
+    thursday = [9,11,21,16,17]
+    avgThursday = thursday.sum
+    friday = [13,15,26,21]
+    avgFriday = friday.sum
+    saturday = [25,20,29,25]
+    avgSaturday = saturday.sum
+    sunday = [29,22,21,22]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
+  def averageOctober:
+    monday = [34,41,27,24]
+    avgMonday = monday.sum
+    tuesday = [31,27,24,23]
+    avgTuesday = tuesday.sum
+    wednesday = [38,35,26,26]
+    avgWednesday = wednesday.sum
+    thursday = [27,24,20,18]
+    avgThursday = thursday.sum
+    friday = [17,30,36,23,25]
+    avgFriday = friday.sum
+    saturday = [27,39,36,30,24]
+    avgSaturday = saturday.sum
+    sunday = [19,24,23,22,19]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
+  def averageNovember:
+    monday = [25,21,25,35,20]
+    avgMonday = monday.sum
+    tuesday = [24,25,26,38,24]
+    avgTuesday = tuesday.sum
+    wednesday = [22,28,24,33]
+    avgWednesday = wednesday.sum
+    thursday = [21,29,24,45]
+    avgThursday = thursday.sum
+    friday = [22,26,20,39]
+    avgFriday = friday.sum
+    saturday = [29,32,30,34]
+    avgSaturday = saturday.sum
+    sunday = [29,25,35,23]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
+  def averageDecember:
+    monday = [23,24,41,46]
+    avgMonday = monday.sum
+    tuesday = [24,20,45,49]
+    avgTuesday = tuesday.sum
+    wednesday = [25,21,25,41,41]
+    avgWednesday = wednesday.sum
+    thursday = [25,25,21,34,41]
+    avgThursday = thursday.sum
+    friday = [24,27,23,45,44]
+    avgFriday = friday.sum
+    saturday = [26,35,30,42]
+    avgSaturday = saturday.sum
+    sunday = [26,28,37,43]
+    avgSunday = sunday.sum
+    monthlyAvg = avgMonday + avgTuesday + avgWednesday + avgWednesday + avgThursday + avgFriday
+    print(monthlyAvg)
+  end
